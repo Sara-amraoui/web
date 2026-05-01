f796fb94733159cea39e4a5c2d7c6c65b458d1fd
Sara-amraoui <s2a0r0a4amra@gmail.com>
2026-05-01 09:10:02 +0100

    Update Flash.php

--- lab3/core/Flash.php
+++ lab3/core/Flash.php
@@ -1,1 +1,22 @@
 
+<?php
+
+declare(strict_types=1);
+
+final class Flash
+{
+    public static function set(string $type, string $message): void
+    {
+        $_SESSION['_flash'][$type][] = $message;
+    }
+
+    /**
+     * @return array<string, list<string>>
+     */
+    public static function get(): array
+    {
+        $f = $_SESSION['_flash'] ?? [];
+        unset($_SESSION['_flash']);
+        return is_array($f) ? $f : [];
+    }
+}
